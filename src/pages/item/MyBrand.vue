d1192a3a245e2b7298d1f6724b55a9c1cc599317
MzMing <459415188@qq.com>
2018-11-13 17:15:38 +0800

    2018-11-13 显示品牌

--- src/pages/item/MyBrand.vue
+++ src/pages/item/MyBrand.vue
@@ -78,44 +78,20 @@
       getDataFromServer() {
         this.loading = true;
 
-
-        const brands = [
-          {
-            "id": 2032,
-            "name": "OPPO",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2119/133/2264148064/4303/b8ab3755/56b2f385N8e4eb051.jpg",
-            "letter": "O"
-          },
-          {
-            "id": 2033,
-            "name": "飞利浦（PHILIPS）",
-            "image": "http://img12.360buyimg.com/popshop/jfs/t18361/122/1318410299/1870/36fe70c9/5ac43a4dNa44a0ce0.jpg",
-            "letter": "F"
-          },
-          {
-            "id": 2034,
-            "name": "华为（HUAWEI）",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t5662/36/8888655583/7806/1c629c01/598033b4Nd6055897.jpg",
-            "letter": "H"
-          },
-          {
-            "id": 2036,
-            "name": "酷派（Coolpad）",
-            "image": "http://img10.360buyimg.com/popshop/jfs/t2521/347/883897149/3732/91c917ec/5670cf96Ncffa2ae6.jpg",
-            "letter": "K"
-          },
-          {
-            "id": 2037,
-            "name": "魅族（MEIZU）",
-            "image": "http://img13.360buyimg.com/popshop/jfs/t3511/131/31887105/4943/48f83fa9/57fdf4b8N6e95624d.jpg",
-            "letter": "M"
+        //ajax请求
+        this.$http.get("/item/brand/page",{
+          params:{
+            page:1,
+            rows:5,
+            sortBy:"id",
+            desc:true,
+            search:this.search,
           }
-        ];
-        setTimeout(() => {
-          this.brands = brands;
-          this.totalBrands = 10;
+        }).then(resp =>{
+          this.brands = resp.data.items;
+          this.totalBrands = resp.data.total;
           this.loading = false;
-        }, 300);
+        })
       }
     }
   }
