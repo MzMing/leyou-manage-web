b958f657e05f1967b74a5d050d793d61c9ca3c74
MzMing <459415188@qq.com>
2018-11-23 09:25:13 +0800

    2018-11-23 商品查询，商品新增已实现，修改数据回显存在问题

--- src/pages/item/MyGoods.vue
+++ src/pages/item/MyGoods.vue
@@ -2,12 +2,13 @@
   <v-card>
     <v-card-title>
       <v-btn color="primary" @click="addGoods">新增商品</v-btn>
+      <v-spacer/>
       <v-flex xs3>
         状态：
         <v-btn-toggle v-model="filter.saleable">
-          <v-btn flat>
-            全部
-          </v-btn>
+          <!--   <v-btn flat :value="true">
+               全部
+             </v-btn>-->
           <v-btn flat :value="true">
             上架
           </v-btn>
@@ -16,6 +17,7 @@
           </v-btn>
         </v-btn-toggle>
       </v-flex>
+      <!--搜索框，与search属性关联-->
       <v-spacer/>
       <!--搜索框，与search属性关联-->
       <v-text-field label="输入关键字搜索" v-model.lazy="filter.search" append-icon="search" hide-details/>
@@ -43,7 +45,7 @@
       </template>
     </v-data-table>
     <!--弹出的对话框-->
-    <v-dialog max-width="500" v-model="show" persistent>
+    <v-dialog max-width="800" v-model="show" persistent scrollable>
       <v-card>
         <!--对话框的标题-->
         <v-toolbar dense dark color="primary">
@@ -53,9 +55,17 @@
           <v-btn icon @click="closeWindow"><v-icon>close</v-icon></v-btn>
         </v-toolbar>
         <!--对话框的内容，表单-->
-        <v-card-text class="px-5">
-          <my-goods-form :oldGoods="oldGoods" />
+        <v-card-text class="px-5" style="height: 600px">
+          <my-goods-form :oldGoods="oldGoods" :step="step" />
         </v-card-text>
+        <!--底部按钮，用来操作步骤线-->
+        <v-card-actions class="elevation-10">
+          <v-flex class="xs3 mx-auto">
+            <v-btn @click="previous" color="primary" :disabled="step === 1">上一步</v-btn>
+            <v-btn @click="next" color="primary" :disabled="step === 4">下一步</v-btn>
+          </v-flex>
+        </v-card-actions>
+
       </v-card>
     </v-dialog>
   </v-card>
@@ -69,6 +79,10 @@
     name: "my-goods",
     data() {
       return {
+        filter:{
+          saleable: false, // 上架还是下架
+          search: '', // 搜索过滤字段
+        },
         search: '', // 搜索过滤字段
         totalGoods: 0, // 总条数
         goodsList: [], // 当前页品牌数据
@@ -84,10 +98,7 @@
         show: false,// 控制对话框的显示
         oldGoods: {}, // 即将被编辑的商品信息
         isEdit: false, // 是否是编辑
-        filter:{
-          saleable: false, // 上架还是下架
-          search: '', // 搜索过滤字段
-        },
+        step:1,
       }
     },
     mounted() { // 渲染后执行
@@ -95,6 +106,12 @@
       this.getDataFromServer();
     },
     watch: {
+      filter: {// 监视搜索字段
+        handler() {
+          this.getDataFromServer();
+        },
+        deep:true
+      },
       pagination: { // 监视pagination属性的变化
         deep: true, // deep为true，会监视pagination的属性及属性中的对象属性变化
         handler() {
@@ -106,12 +123,6 @@
         handler() {
           this.getDataFromServer();
         }
-      },
-      filter: {// 监视搜索字段
-        handler() {
-          this.getDataFromServer();
-        },
-        deep:true
       }
     },
     methods: {
@@ -131,6 +142,16 @@
           this.loading = false;
         })
       },
+      previous(){
+        if(this.step > 1){
+          this.step--
+        }
+      },
+      next(){
+        if(this.step < 4){
+          this.step++
+        }
+      },
       addGoods() {
         // 修改标记
         this.isEdit = false;
@@ -139,7 +160,9 @@
         // 把oldBrand变为null
         this.oldBrand = null;
       },
-      editGoods(oldGoods){
+      async editGoods(oldGoods){
+        oldGoods.spuDetail = await this.$http.loadData("/item/spu/detail/" + oldGoods.id);
+        oldGoods.skus = await this.$http.loadData("/item/sku/list?id=" + oldGoods.id);
         // 修改标记
         this.isEdit = true;
         // 控制弹窗可见：
@@ -153,9 +176,7 @@
         // 关闭窗口
         this.show = false;
       }
-
-
-    }, // methods
+    },
     components:{
       MyGoodsForm
     }
